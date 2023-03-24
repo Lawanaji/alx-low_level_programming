94cb23fa8d76d6108e5310708ff7e1eb5606f3c9
Lawanaji <lawanmustaphaaji2015@gmail.con>
2023-03-23 21:08:44 -0700

    print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,6 +4,7 @@
  * print_numbers - print num from 0-9
  * Return: void
  */
+
 void print_numbers(void)
 {
 	char c;
@@ -12,5 +13,5 @@ void print_numbers(void)
 	{
 		_putchar(c);
 	}
-	_putchar ('\n')
+	_putchar ('\n');
 }
