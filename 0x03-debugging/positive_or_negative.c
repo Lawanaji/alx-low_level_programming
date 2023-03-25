9808699e318897f98d3509556e87f1357315ae6b
Lawanaji <lawanmustaphaaji2015@gmail.con>
2023-03-24 17:36:59 -0700

    main

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,7 +3,7 @@
 void positive_or_negative(int i)
 {
 	if (i < 0)
-		printf("%d is negative", i);
+		printf("%d is negative\n", i);
 	else
-		printf("%d is positve", i);
+		printf("%d is positve\n", i);
 }
